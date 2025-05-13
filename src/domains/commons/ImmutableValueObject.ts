a0a9dd2f68fee6a527da20e74dba68a29cf9bdf7
maskatcc <masaaki.katou@gmail.com>
2025-05-14 07:55:41 +0900

    coding base value object

---
+++ src/domains/commons/ImmutableValueObject.ts
@@ -0,0 +1,19 @@
+export class ImmutableValueObject<T> {
+    public readonly value: T;
+
+    constructor(value: T) {
+      this.value = value;
+    }
+
+    equals(other: ImmutableValueObject<T>): boolean {
+      if (this === other) {
+        return true;
+      }
+
+      if (other === null || other === undefined) {
+        return false;
+      }
+
+      return this.value === other.value;
+    }
+}
