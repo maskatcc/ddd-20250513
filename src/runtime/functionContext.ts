0d1cb9fcd04d46010d73985015c735cba34b676e
maskatcc <masaaki.katou@gmail.com>
2025-05-17 21:11:48 +0900

    prepare postgresql context

--- src/runtime/functionContext.ts
+++ src/runtime/functionContext.ts
@@ -1,10 +1,13 @@
 import { KeycloakLib } from './keycloakLib.js'
+import { PostgresqlLib } from './postgresqlLib.js'
 
 type FunctionContext = {
   Keycloak: KeycloakLib
+  Postgresql?: PostgresqlLib
 }
 
 export {
   FunctionContext,
   KeycloakLib,
+  PostgresqlLib,
 }
