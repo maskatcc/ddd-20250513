2d1ddaadaa3eb2ddc0e858d2fc73800781592761
maskatcc <masaaki.katou@gmail.com>
2025-05-17 21:13:10 +0900

    coding postgresql infrastructure

---
+++ src/infrastructures/postgresql/userQueryRepository.ts
@@ -0,0 +1,23 @@
+import { OrganizationId } from '../../domains/organization/organization.js'
+import { FunctionContext, PostgresqlLib } from '../../runtime/functionContext.js'
+
+export type QueryUserRecord = {
+  userId: string
+  userName: string
+  email: string
+}
+
+export class UserQueryRepository {
+  readonly postgresql: PostgresqlLib
+
+  constructor(private readonly context: FunctionContext) {
+    if ((process.env.NODE_ENV || process.env.DB_ENV) && !context.Postgresql) {
+      throw new Error('Postgresql is not initialized.')
+    }
+    this.postgresql = context.Postgresql!
+  }
+
+  async query(_orgId: OrganizationId): Promise<QueryUserRecord[]> {
+    throw new Error('Method not implemented.')
+  }
+}
